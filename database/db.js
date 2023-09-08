fa5c8858ac2a61ff647c722e0ef757f03e6bc667
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2023-09-08 13:20:31 +0100

    Change date to correct format

--- database/db.js
+++ database/db.js
@@ -1,10 +1,14 @@
 const moment= require('moment');
 
+const getTime= moment().utc()
+
+const formattedTime = getTime.toISOString().split(".")[0] + 'Z';
+
 
 const data = {
         slack_name: 'Adeyinka Oresanya',
         current_day: moment().format('dddd'),
-        utc_time: moment().utc(),
+        utc_time: formattedTime,
         track: 'backend',
         github_file_url: 'https://github.com/adeyinkaoresanya/hng-stage-one-task/blob/main/server.js',
         github_repo_url: 'https://github.com/adeyinkaoresanya/hng-stage-one-task',
