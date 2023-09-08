c077103d2e2f8bd08545ead9971338e064e2a9b5
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2023-09-08 12:32:18 +0100

    Create a database file

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const moment= require('moment');
+
+
+const data = {
+        slack_name: 'Adeyinka Oresanya',
+        current_day: moment().format('dddd'),
+        utc_time: moment().utc(),
+        track: 'backend',
+        github_file_url: 'bbbb',
+        github_repo_url: 'https://github.com/adeyinkaoresanya/hng-stage-one-task',
+        status_code: 200,
+    }
+    
+module.exports = data;
